41677f4d78111cfe698b3261eabf4c651756460b
Visshm <visshwa.magesh@gmail.com>
2024-02-26 21:44:18 -0500

    junit1

--- src/fitness/DateTest.java
+++ src/fitness/DateTest.java
@@ -4,16 +4,53 @@ import static org.junit.Assert.*;
 
 public class DateTest {
 
-    @org.junit.Test
-    public void testDaysInFeb_NonLeap() {
-        Date date = new Date("2/30/2013");
-        assertFalse(date.isValid());
-    }
-
+    // Valid Case 1
     @org.junit.Test
     public void testDaysInFeb_Leap(){
         Date date = new Date("2/29/2024");
         assertTrue(date.isValid());
+    }
+
+    // Valid Case 2
+    @org.junit.Test
+    public void testDaysInFeb_Leap1(){
+        Date date = new Date("2/29/2000");
+        assertTrue(date.isValid());
+    }
+
+    // Invalid Case 1
+    @org.junit.Test
+    public void testDaysInFeb_NonLeap() {
+        Date date = new Date("2/29/1987");
+        assertFalse(date.isValid());
+    }
+
+    // Invalid Case 2
+    @org.junit.Test
+    public void testDaysInFeb_NonLeap1() {
+        Date date = new Date("2/29/2015");
+        assertFalse(date.isValid());
+    }
+
+    // Invalid Case 3
+    @org.junit.Test
+    public void testDaysInFeb_NonLeap2() {
+        Date date = new Date("2/29/2007");
+        assertFalse(date.isValid());
+    }
+
+    // Invalid Case 4
+    @org.junit.Test
+    public void testDaysInFeb_NonLeap3() {
+        Date date = new Date("2/29/2019");
+        assertFalse(date.isValid());
+    }
+
+    // Invalid Case 5
+    @org.junit.Test
+    public void testDaysInFeb_NonLeap4() {
+        Date date = new Date("2/29/1645");
+        assertFalse(date.isValid());
     }
 
 }
